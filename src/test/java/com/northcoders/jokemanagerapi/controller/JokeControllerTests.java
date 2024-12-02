243f99a1c7ce2b0d360d08eb747a08d70d94444a
ateki <irene.atek@btinternet.com>
2024-12-02 12:04:43 +0000

    feat: impl tests for getJokes in JokeControllerTests

--- src/test/java/com/northcoders/jokemanagerapi/controller/JokeControllerTests.java
+++ src/test/java/com/northcoders/jokemanagerapi/controller/JokeControllerTests.java
@@ -1,6 +1,7 @@
 package com.northcoders.jokemanagerapi.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.jokemanagerapi.model.Joke;
 import com.northcoders.jokemanagerapi.service.JokeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 
 @AutoConfigureMockMvc
@@ -39,6 +48,30 @@ class JokeControllerTests {
     @Test
     @DisplayName("GET /jokes")
     void getJokes() throws Exception {
+        Instant createdAt = Instant.parse("2021-02-09T11:19:42.12Z");
+        Instant modifiedAt = Instant.parse("2021-02-09T15:20:42.12Z");
 
+
+
+        List<Joke> jokes = new ArrayList<Joke>();
+        jokes.add(
+                new Joke(1L, "setupLine1", "punchLine1", Joke.JokeCategories.PUN, true,  createdAt, modifiedAt),
+                new Joke(2L, "setupLine2", "punchLine2", Joke.JokeCategories.PROGRAMMING, true,  createdAt, modifiedAt)
+
+        );
+
+        // when we call mockService's method ??? we want it to return list of jokes
+        when(mockJokeServiceImpl.getAllJokes()).thenReturn(jokes);
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/jokes"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].setupLine1").value("setupLine1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].punchLine1").value("punchLine1"))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].setupLine2").value("setupLine2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].punchLine2").value("punchLine2"));
+                // TODO: Add checks for remaining attributes of Joke Entity
     }
 }
